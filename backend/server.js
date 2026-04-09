055295fe14aa377ec4b5140fe3dd7c61c4be1ce9
Josiah Walutsachi <jwalutsachi4@gmail.com>
2026-04-09 11:59:20 +0300

    fix: add missing origin URL for CORS in server configuration

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://oncology-nutrition-space.vercel.app',
+    'https://onologynutritionspace.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
